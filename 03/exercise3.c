7e8146d01a9bbc27ac7c7b10767605e74d596422
Pal M Sivertsen <palmarius@hotmail.com>
2014-09-10 17:03:37 +0200

    Added execrise 3

---
+++ 03/exercise3.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+int main (int argc, char * argv[]) {
+  if (argc < 2) {
+    printf("Missing args!");
+    return -1;
+  }
+  int i = atoi(argv[1]);
+  if (!i) {
+    printf("Input nonzero value!");
+    return -1;
+  }
+  if (i % 2) {
+    printf("%d is odd!", i);
+  } else {
+    printf("%d is even!", i);
+  }
+  return 0;
+}
